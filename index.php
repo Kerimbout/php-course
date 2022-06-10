e6c82607191f5192ba21afa442b1402fa95f8d35
Kerimbout <52263565+Kerimbout@users.noreply.github.com>
2022-06-10 06:52:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -15,31 +15,27 @@
       $products['Car']=15000;
       $products['iPhone']=1000;
       $products['Toaster']=75;
-
-      foreach($products as $key => $value)
-      {
-	      echo "<p>The ".$key." costs ".$value."</p>";
-      }
-      echo "<h2>Items you can afford</h2>";
-      foreach($products as $key => $value)
-      {
-	      if($value <= $wallet )
-	      {
-		      echo "<p>".$key."</p>"; 
-	      }
-      }
-	  
-      $amount=800;
+ 
       $taxRate=0.0825;
-      $addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
-      echo $addedTax;
-	  
-      function tax_calc($amount,$tax)
-      {
-  	$calculate_tax = $amount*$tax;
-  	$amount = round($amount+$calculate_tax,2);
-  	return $amount;
-      }
+     function tax_calc($amount,$tax){
+	    	$addedTax = $amount*$tax;
+	    	$amountWithTax = round($amount+$addedTax,2);
+	    	return $amountWithTax;
+    	}
+
+	    foreach($products as $key => $value){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
+	    }
+
+	    echo "<h2>Items you can afford</h2>";
+
+	    foreach($products as $key => $value){
+	    	$costWithTax = tax_calc($value,$taxRate);
+		    if($costWithTax <= $wallet ){
+		    	echo "<p>".$key."</p>"; 
+		    }
+	    }
     ?>
   </body>
 </html>
