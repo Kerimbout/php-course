ff57ef6babe4ba27ee43ca0d9411305cc00c343a
Kerimbout <52263565+Kerimbout@users.noreply.github.com>
2022-06-10 06:43:41 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,22 @@
       echo "<h2>You have $".$wallet." in your wallet.</h2>";
     
       $products['Computer']=750;
-	    $products['Car']=15000;
-	    $products['iPhone']=1000;
-	    $products['Toaster']=75;
+      $products['Car']=15000;
+      $products['iPhone']=1000;
+      $products['Toaster']=75;
 
-	    foreach($products as $key => $value){
-	    echo "<p>The ".$key." costs ".$value."</p>";
-    }
+      foreach($products as $key => $value)
+      {
+	      echo "<p>The ".$key." costs ".$value."</p>";
+      }
+      echo "<h2>Items you can afford</h2>";
+      foreach($products as $key => $value)
+      {
+	      if($value <= $wallet )
+	      {
+		      echo "<p>".$key."</p>"; 
+	      }
+      }
     ?>
   </body>
 </html>
