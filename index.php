9dc2886031a2e1f9456fcdc9c5ba7609900a3303
Kerimbout <52263565+Kerimbout@users.noreply.github.com>
2022-06-10 06:29:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,14 @@
 
       echo "<h1>Welcome to ".$name."!</h1>";
       echo "<h2>You have $".$wallet." in your wallet.</h2>";
+    
+      $products['Keyboard']=750;
+	    $products['Laptop']=15000;
+	    $products['Samsung Phone']=1000;
+	    $products['Charger']=75;
+
+	    echo "<p>A laptop costs $".$products['Laptop']."</p>";
+    ?>
     ?>
   </body>
 </html>
